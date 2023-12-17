45e62a5518632ad13e7e2ffda95d8ecd638d26c7
Lencamo <207141481@qq.com>
2023-12-17 11:04:13 +0800

    node端：.env环境说明

--- order-manage_node/src/config/dotenv.js
+++ order-manage_node/src/config/dotenv.js
@@ -5,24 +5,15 @@ if (result.error) {
   throw result.error
 }
 
-const {
-  APP_PORT,
-  APP_HOST,
-  EXPIRES_IN,
-  MYSQL_HOST,
-  MYSQL_USER,
-  MYSQL_DATABASE,
-  MYSQL_PASSWORD,
-  UPLOAD_PATH
-} = process.env
+const { APP_PORT, APP_HOST, EXPIRES_IN, WX_APPID, WX_APPSECRET, CLOUD_ENV, UPLOAD_PATH } =
+  process.env
 
 module.exports = {
   APP_PORT,
   APP_HOST,
   EXPIRES_IN,
-  MYSQL_HOST,
-  MYSQL_USER,
-  MYSQL_DATABASE,
-  MYSQL_PASSWORD,
+  WX_APPID,
+  WX_APPSECRET,
+  CLOUD_ENV,
   UPLOAD_PATH
 }
