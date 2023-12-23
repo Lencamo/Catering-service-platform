dbfccae242750a6e4a76c9175ea7666e83de41bf
Lencamo <207141481@qq.com>
2023-12-23 10:16:23 +0800

    node端：multer中间件优化

--- order-manage_node/src/middleware/file/upload.middleware.js
+++ order-manage_node/src/middleware/file/upload.middleware.js
@@ -1,14 +1,28 @@
+const fs = require('fs')
 const { UPLOAD_PATH } = require('../../config/dotenv.js')
+
 const multer = require('@koa/multer')
 
 const upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
+      // 确保dest文件存在
+      if (!fs.existsSync(UPLOAD_PATH)) {
+        fs.mkdirSync(UPLOAD_PATH)
+      }
+
       cb(null, UPLOAD_PATH)
     },
     filename: function (req, file, cb) {
+      // unique后缀
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+
+      // 文件名后缀
+      const fileFormat = file.originalname.split('.')
+      const picSuffix = fileFormat[fileFormat.length - 1]
+
+      // cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, file.fieldname + '-' + uniqueSuffix + '.' + picSuffix)
     }
   })
 })
